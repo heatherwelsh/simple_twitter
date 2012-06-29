962c7b3fe330ca89a12016c1e1e2e70483ea1693
Walt <moabwh@gmail.com>
2012-06-29 11:55:50 -0600

    Update master

--- test/search_test.rb
+++ test/search_test.rb
@@ -1,9 +1,23 @@
-class MockExampleTest < Test::Unit::TestCase
-  def test_example_mocking
+################################################################################
+require('test/unit')
+require('mocha')
+require('simple_twitter')
 
-    file_name = File.expand_path('--/json')
+################################################################################
+class SearchTest < Test::Unit::TestCase
 
-end
+  ##############################################################################
+  def test_search_method
+    # Don't talk with Twitter, use a file instead
+    json_file = File.expand_path('gaga.json', File.dirname(__FILE__))
+    response = stub('response', :body => File.read(json_file))
+    HTTParty.expects(:get).returns(response)
 
+    # Test SimpleTwitter::Search#search
+    searcher = SimpleTwitter::Search.new
+    tweets = searcher.search("doesn't matter")
 
+    assert(tweets.is_a?(Array))
+    assert(tweets.all? {|t| t.is_a?(SimpleTwitter::Tweet)})
+  end
 end
